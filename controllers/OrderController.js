fee36f1dfe5726a8272cc1c304b35afa1154b59f
Varun-Rubussoft <varun@rubussoft.com>
2024-06-01 03:07:03 +0530

    INr

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -62,7 +62,7 @@ exports.createPaymentIntent = async (req, res) => {
     // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount * 100, // Stripe amount is in cents/pence
-      currency: 'usd',
+      currency: 'inr',
       automatic_payment_methods: {
         enabled: true,
       },
